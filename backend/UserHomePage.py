7f0a43ee421b3542e4f34be1d9054528e6d50857
IsHanSolo9000 <53462059+IsHanSolo9000@users.noreply.github.com>
2023-03-06 21:34:37 -0600

    Update UserHomePage.py
    So far a skeleton class. Created seperate backend class for User Homepage which is a hub where user accesses databases. Seperate class from the login backend because other login.py would be too large

--- backend/UserHomePage.py
+++ backend/UserHomePage.py
@@ -1,1 +1,19 @@
+import sys
+import json
+
+from bson import ObjectId
+from getpass import getpass
+from bson.json_util import dumps
+from pymongo import MongoClient
+import pymongo
+class UserHomePage:
+
+
+def AccessHardware(self):
+
+
+def Projects(self):
+
+def ProfileInfo(self):
+    
 
