5c80038053c4060156ffb8502b727653ab5a8ca7
Adal Ordonez <adal.ordonez@utexas.edu>
2023-03-15 13:30:00 -0500

    Deleted repetitive and misplaced frontend React files from repository

--- Team-Projects-Website/src/index.js
+++ Team-Projects-Website/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Popup from 'react-popup'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Popup />
     <App />
   </React.StrictMode>
 );
